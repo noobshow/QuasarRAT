ec2e5f45149ad1259ec030bd0cce6ac3b665d316
Justin Yanke <yankejustin@hotmail.com>
2015-10-07 12:51:15 -0300

    Behavior changes for Registry Editor Form
    Fixed a few behavior issues with the Registry Editor Form, added some
    documentation, and added some notes in preparation for the fixes to the
    way RegistryKeys are added to the TreeView. Also added some notes for
    the upcoming implementation to obtain the values of the RegistryKeys.

--- Server/Forms/FrmRegistryEditor.cs
+++ Server/Forms/FrmRegistryEditor.cs
@@ -19,9 +19,6 @@ namespace xServer.Forms
             _connectClient.Value.FrmRe = this;
 
             InitializeComponent();
-
-            // By passing no search arguments, we tell the client to provide us with a response of its root keys.
-            new xServer.Core.Packets.ServerPackets.DoLoadRegistryKey(null).Execute(_connectClient);
         }
 
         public TreeNode AddRootKey(RegSeekerMatch match)
@@ -35,7 +32,7 @@ namespace xServer.Forms
 
                 node.Nodes.Add(new TreeNode());
             });
-            
+
             return node;
         }
 
@@ -50,7 +47,7 @@ namespace xServer.Forms
 
                 node.Nodes.Add(new TreeNode());
             });
-            
+
             return node;
         }
 
@@ -66,8 +63,20 @@ namespace xServer.Forms
 
         public void AddKeyToTree(RegSeekerMatch match)
         {
+            if (string.IsNullOrEmpty(match.Key))
+            {
+                // The key can be whitespace, but not null or empty...
+                return;
+            }
+
             TreeNode parent = GetParentTreeNode(match);
-            if (parent != null)
+
+            // If the parent is null, it should be a root node.
+            if (parent == null)
+            {
+                AddRootKey(match);
+            }
+            else
             {
                 // ToDo: Add more on the object's tag.
                 parent.Nodes.Add(CreateNode(match.Key, match.Key, match.Data));
@@ -83,41 +92,48 @@ namespace xServer.Forms
         /// itself if it is a root match.</returns>
         private TreeNode GetParentTreeNode(RegSeekerMatch match)
         {
-            if (string.IsNullOrEmpty(match.Key) || string.IsNullOrEmpty(match.Key.Trim()))
-            {
-                return null;
-            }
-            else if (match.Key.Contains("/"))
+            if (match.Key.Contains("/"))
             {
                 // It might not be a root node.
                 string[] nodePath = match.Key.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
 
-                // Only one valid node. Probably malformed.
+                // Only one valid node. Probably malformed or a root node.
                 if (nodePath.Length < 2)
                 {
                     return null;
                 }
                 else
                 {
+                    // Keep track of the last node to reference for traversal.
                     TreeNode lastNode = null;
+
+                    // If the TreeView contains the first element in the node path, we
+                    // won't have to create it. Otherwise, create it then set the last node
+                    // to serve as our reference point.
                     if (tvRegistryDirectory.Nodes.ContainsKey(nodePath[0]))
                     {
                         lastNode = tvRegistryDirectory.Nodes[nodePath[0]];
                     }
                     else
                     {
+                        // This node does not exist in the TreeView. Create it then add it.
                         lastNode = CreateNode(nodePath[0], nodePath[0], null);
                         tvRegistryDirectory.Nodes.Add(lastNode);
                     }
 
+                    // Go through the rest of the node path.
                     for (int i = 1; i < nodePath.Length; i++)
                     {
+                        // If the last node does have this entry in the path, just set
+                        // the last node to the existing entry.
                         if (lastNode.Nodes.ContainsKey(nodePath[i]))
                         {
                             lastNode = tvRegistryDirectory.Nodes[nodePath[i]];
                         }
                         else
                         {
+                            // If the last node does not contain the next item in the path,
+                            // create the node and add it to the path.
                             TreeNode newNode = CreateNode(nodePath[i], nodePath[i], null);
                             lastNode.Nodes.Add(newNode);
 
@@ -131,7 +147,7 @@ namespace xServer.Forms
             else
             {
                 // Likely a root node.
-                return AddRootKey(match);
+                return null;
             }
         }
 
@@ -164,22 +180,39 @@ namespace xServer.Forms
         {
             if ((tvRegistryDirectory.SelectedNode != null) && (tvRegistryDirectory.SelectedNode.Tag != null))
             {
+                // Step One
+                // Set a class-level variable based on the full RegistryKey path to the
+                // RegistryKey we wish to obtain the value of.
+                // To-Do: Currently, we do not use the full path to the RegistryKey for the TreeNode.
+                //       Such behavior needs to be changed for this to work because we need the full path.
+                // ---
+                /*
+                 Pseudo:
+                 
+                 class-level variable to get the value of (RegistryKey path) =
+                 * tvRegistryDirectory.SelectedNode.Name;
+                 */
+
+                // Step Two
+                // Send a packet containing the path to the RegistryKey to obtain the value of.
+                // ---
+
+                // Step Three
+                // Upon a response, if the RegistryKey path does not match, do not bother attempting
+                // to use the data. If it does, use this code below to populate the ListView with the
+                // data that the client provided to us.
+                // ---
+
                 List<RegistryKey> KeysFromNode = new List<RegistryKey>();
-                
-                // Send a request for registry keys on this node.
-
-                // ToDo: (After 
-
-                // ToDo: Move the code below in a spot that the RegistryHandler will make use of
                 if (KeysFromNode.Count < 1)
-                {
-                    // If there are valid keys to use from the selected TreeNode, populate the ListView with it.
-                    PopulateLstRegistryKeys(KeysFromNode.ToArray());
-                }
-                else
                 {
                     // If there aren't, send null to clear the ListView.
                     PopulateLstRegistryKeys(null);
+                }
+                else
+                {
+                    // If there are valid keys to use from the selected TreeNode, populate the ListView with it.
+                    PopulateLstRegistryKeys(KeysFromNode.ToArray());
                 }
             }
             else
@@ -192,7 +225,8 @@ namespace xServer.Forms
 
         private void FrmRegistryEditor_Load(object sender, EventArgs e)
         {
-            // Send packet to obtain the root nodes.
+            // By passing no search arguments, we tell the client to provide us with a response of its root keys.
+            new xServer.Core.Packets.ServerPackets.DoLoadRegistryKey(null).Execute(_connectClient);
         }
 
         private void FrmRegistryEditor_FormClosing(object sender, FormClosingEventArgs e)
@@ -203,7 +237,7 @@ namespace xServer.Forms
 
         private void tvRegistryDirectory_AfterLabelEdit(object sender, NodeLabelEditEventArgs e)
         {
-            // After the Tree Node label text is edited, 
+
         }
 
         private void tvRegistryDirectory_AfterSelect(object sender, TreeViewEventArgs e)
@@ -231,12 +265,12 @@ namespace xServer.Forms
                     if (key != null)
                     {
                         // Get sub-keys, sort them, then add them to the tree.
-                        
+
                     }
                 }
                 finally
                 {
-                    ResumeLayout();
+                    tvRegistryDirectory.ResumeLayout();
                 }
             }
         }
